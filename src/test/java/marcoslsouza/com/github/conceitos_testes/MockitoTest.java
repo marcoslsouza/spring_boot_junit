e9a331bbb14cfe25809c6dac5fd0cc132ff5fee0
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-17 03:08:47 -0300

    Outra forma de criar um Mock

--- src/test/java/marcoslsouza/com/github/conceitos_testes/MockitoTest.java
+++ src/test/java/marcoslsouza/com/github/conceitos_testes/MockitoTest.java
@@ -1,19 +1,33 @@
 package marcoslsouza.com.github.conceitos_testes;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //import org.junit.jupiter.api.Assertions;
 import static org.assertj.core.api.Assertions.assertThat;
+
+// import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class MockitoTest {
-
+	
+	@Mock
+	List<String> lista;
+	
+	// Roda BeforeEach() antes de todos os metodos.
+	@BeforeEach
+	public void BeforeEach() {
+		// O Mockito ler todos os atributos @Mock desta classe e cria os Mocks.
+		MockitoAnnotations.openMocks(this);
+	}
+	
 	@Test
 	public void primeiroTesteMockito() {
 		
-		List<String> lista = Mockito.mock(ArrayList.class);
+		// List<String> lista = Mockito.mock(ArrayList.class);
 		
 		Mockito.when(lista.size()).thenReturn(20);
 		
