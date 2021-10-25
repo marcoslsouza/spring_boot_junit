fc22cd5fcbf2b6389c916725255719a7453bd525
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-25 09:02:57 -0300

    Criando os endpoins de teste

--- src/main/java/marcoslsouza/com/github/conceitos_testes/controller/MeuResource.java
+++ src/main/java/marcoslsouza/com/github/conceitos_testes/controller/MeuResource.java
@@ -1,7 +1,13 @@
 package marcoslsouza.com.github.conceitos_testes.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +25,20 @@ public class MeuResource {
 		Cliente cliente = new Cliente("Fulano", "000.000.000-00");
 		
 		return cliente;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente) {
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, Cliente cliente) {
+		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete() {
+		return ResponseEntity.noContent().build();
 	}
 }
