d7c2007e9d4c752c425d32021bb5e0e3a67e3dd2
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-30 17:11:25 -0300

    Desenhando a requisicao TDD para livros (POST)

--- src/test/java/marcoslsouza/com/github/conceitos_testes/livros/LivrosControllerTest.java
+++ src/test/java/marcoslsouza/com/github/conceitos_testes/livros/LivrosControllerTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /* Teste unitario */
 
@@ -25,6 +32,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class LivrosControllerTest {
 	
+	// Definir a rota. Rota definida no controller
+	static String LIVROS_API = "/api/livros";
+	
 	@Autowired
 	// MockMvc simula como se fosse uma requisicao para a API
 	MockMvc mvc;
@@ -32,8 +42,24 @@ public class LivrosControllerTest {
 	@Test
 	@DisplayName("Deve criar um livro com sucesso")
 	// Primeiro teste para a requisicao POST
-	public void criarLivroTest() {
+	public void criarLivroTest() throws Exception {
 		
+		String json = new ObjectMapper().writeValueAsString(null);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+			.post(LIVROS_API)
+			.contentType(MediaType.APPLICATION_JSON) // Sera enviado JSON
+			.accept(MediaType.APPLICATION_JSON) // Servidor aceita JSON
+			.content(json); // JSON passado
+		
+		// Fazer a requisicao
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isCreated()) // Verifica o status recebido
+			.andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty()) // Verifica se o JSON de resposta e igual ao enviado. Verifica se o id nao esta vazio
+			.andExpect(MockMvcResultMatchers.jsonPath("titulo").value("Meu Livro"))
+			.andExpect(MockMvcResultMatchers.jsonPath("autor").value("Autor"))
+			.andExpect(MockMvcResultMatchers.jsonPath("isbn").value("12132222"));
 	}
 	
 	@Test
